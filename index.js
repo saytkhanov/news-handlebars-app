999167211e4e9fd82d51fbf208283694ef2cd941
saytkhanov <81220037+saytkhanov@users.noreply.github.com>
2021-07-30 00:32:38 +0300

    вывел категории и сделал вывод основных новостей, главную новость вывел с помощью метода split

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const hbs = require('express-handlebars');
+const cors = require("cors");
+const hbs = require("express-handlebars");
 
+const data = require("./data");
 
 app.use(cors());
-app.set('view engine', 'handlebars');
-app.engine('handlebars', hbs());
+app.set("view engine", "handlebars");
+app.engine("handlebars", hbs());
 
-
+app.get("/", (req, res) => {
+  res.render("home", {
+    mainNews: data.news[0],
+    secondNews: data.news.slice(1, 3),
+    headers: data.categories,
+  });
+});
 
 app.listen(3030, () => {
-  console.log('Server has been started...')
-})
+  console.log("Server has been started...");
+});
