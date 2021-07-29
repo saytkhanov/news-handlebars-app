3e1791e0acdc71caebf070dadc94925aa54cbe87
saytkhanov <81220037+saytkhanov@users.noreply.github.com>
2021-07-30 00:37:18 +0300

    вывод новостей по категориям, вывод определенной новости и т.д

--- index.js
+++ index.js
@@ -17,6 +17,33 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get('/api/data', (req, res) => {
+
+  res.json(data)
+})
+
+
+app.get('/categoryId/:id', (req, res) => {
+  const dataNews = data.news.filter(item => item.categoryId === Number(req.params.id))
+  console.log(dataNews)
+  res.render("home", {
+    mainNews: dataNews[0],
+    secondNews: dataNews.slice(1, 3),
+    headers: data.categories
+  })
+})
+
+app.get('/contacts', (req, res) => {
+  res.render("contacts")
+})
+
+app.get('/news/:id', (req, res )=> {
+  const oneNews = data.news.find(item =>item.id === Number(req.params.id))
+  res.render('news', {
+    oneNews
+  })
+})
+
 app.listen(3030, () => {
   console.log("Server has been started...");
 });
